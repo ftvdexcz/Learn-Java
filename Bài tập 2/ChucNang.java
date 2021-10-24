31c0e35c39325d726926c93d6f378f1a6a81431b
Đặng Quốc Long <60183306+ftvdexcz@users.noreply.github.com>
2021-10-24 19:23:21 +0700

    Delete ChucNang.java

--- Bài tập 2/ChucNang.java
+++
@@ -1,9 +0,0 @@
-package Q6;
-
-public interface ChucNang {
-    public void themGDVang();
-    public void themGDTienTe();
-    public void themGDDat();
-    public void hienThiGD(); 
-    public void hienThiGDTheoNgay();
-}
