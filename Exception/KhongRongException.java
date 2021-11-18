240ed37d7d4559eac5acdbf4425776e3b375f98c
Đặng Quốc Long <60183306+ftvdexcz@users.noreply.github.com>
2021-11-18 21:23:20 +0700

    Add files via upload

---
+++ Exception/KhongRongException.java
@@ -0,0 +1,7 @@
+package mainPackage;
+
+public class KhongRongException extends Exception{
+    public KhongRongException(String msg) {
+        super(msg);
+    }
+}
---
+++ Exception/SoDuongException.java
@@ -0,0 +1,8 @@
+package mainPackage;
+
+public class SoDuongException extends Exception{
+
+    public SoDuongException(String msg) {
+        super(msg);
+    }
+}
