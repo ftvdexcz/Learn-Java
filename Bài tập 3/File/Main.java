f4bd473371fc400943bd25656348968ace7f75e9
Đặng Quốc Long <60183306+ftvdexcz@users.noreply.github.com>
2021-11-21 18:51:14 +0700

    Add files via upload

--- Bài tập 3/File/Main.java
+++ Bài tập 3/File/Main.java
@@ -3,13 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package baitapfile.bai1;
+package baitapfile.bai2;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -18,28 +15,20 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
-        File f = new File("src/baitapfile/bai1/bai1.txt");
+        File f = new File("src/baitapfile/bai2/bai2.txt");
         Scanner in;
         try{
             in = new Scanner(f);
-            int n = in.nextInt(), m = in.nextInt();
-            int[][] a = new int[n][m];
+            int n = in.nextInt();
+            int[][] a = new int[n][n];
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
+                for (int j = 0; j < n; j++) {
                     a[i][j] = in.nextInt();
                 }
             }
-            MaTranChuNhat m1 = new MaTranChuNhat(n, m);
-            MaTranChuNhat m2 = new MaTranChuNhat(n, m);
-            m1.nhapMaTran(a);
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    a[i][j] = in.nextInt();
-                }
-            }
-            m2.nhapMaTran(a);
-            m1.xuat();
-            m2.xuat();
+            MaTranVuong m = new MaTranVuong(n);
+            m.nhap(a);
+            m.xuat();
             
         }catch(FileNotFoundException e){
             System.out.println(e);
